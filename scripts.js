699545c890851d72af9e48324240114af263ca3f
Stefano <sena.stefano@gmail.com>
2022-10-21 10:15:11 -0400

    Tried and failed again to get checkbox output to work

--- scripts.js
+++ scripts.js
@@ -1,61 +1,8 @@
+let btnOutput = document.querySelector('button');
+let output =  document.querySelector('h1');
 
-
-// Show image taggs in output
-let btnCalidon = document.querySelector('#Calidon');
-
-btnCalidon.addEventListener('change', (event) => {
-    if (event.target.checked) {
-        document.querySelector('.output').innerHTML = + 'Calidon ';
-    }
+btnOutput.addEventListener('click', () => {
+    let checkbox = docuement.querySelector('input[type="checkbox"]:checked');
+    output.innerText = checkbox.value;
 });
-
-// let btnPrimacare = document.querySelector('#Primacare™ Wingback');
-
-// btnPrimacare.addEventListener('change', (event) => {
-//     document.querySelector('.output').innerHTML = + 'Primacare™ Wingback ';
-// });
-
-// let btnAubra = document.querySelector('#Aubra');
-
-// btnAubra.addEventListener('change', (event) => {
-//     document.querySelector('.output').innerHTML = + 'Aubra ';
-// });
-
-// let btnFrolick = document.querySelector('#Frolick');
-
-// btnFrolick.addEventListener('change', (event) => {
-//     document.querySelector('.output').innerHTML = + 'Frolick ';
-// });
-
-
-// // Clear output
-// let btnReset = document.querySelector('#reset');
-
-// btnReset.addEventListener('click', (event) => {
-//     document.querySelector('.output').innerHTML = ' ';
-// });
-
-
-
-
-// tagging.addEventListener("change", checktag);
-
-// function checktag() {
-//     if (tagging.S1.checked) {
-//         output.innerHTML = tagging.S1.value;
-//     } else {
-//         output.innerHTML = '';
-//     }
-// };
-
-
-// function showOutput() {
-//     if (document.getElementById('S1').checked) {
-//       document.getElementById('S1').innerHTML = "S1";
-//     } else {
-//       document.getElementById('S1').innerHTML = "";
-//     }
-//   }
-
-
 
