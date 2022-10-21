cfb7107ac2655217ed61e9bcec2aca96126763e8
Stefano <sena.stefano@gmail.com>
2022-10-21 14:48:45 -0400

    Tried another type of script and failed

--- scripts.js
+++ scripts.js
@@ -1,8 +1,19 @@
-let btnOutput = document.querySelector('button');
-let output =  document.querySelector('h1');
+// let btnOutput = document.querySelector('button');
+// let output =  document.querySelector('h1');
 
-btnOutput.addEventListener('click', () => {
-    let checkbox = docuement.querySelector('input[type="checkbox"]:checked');
-    output.innerText = checkbox.value;
-});
+// btnOutput.addEventListener('click', () => {
+//     let checkbox = docuement.querySelector('input[type="checkbox"]:checked');
+//     output.innerText = checkbox.value;
+// });
 
+
+
+function changeText(){
+    const userInputFurniture = Array(document.querySelectorAll(".furniture:checked")).map(function(el) {
+            return el.value;
+        }).join(' ')
+    
+
+    document.getElementById('submit').innerHTML = userInputFurniture;
+    return false;
+    }
