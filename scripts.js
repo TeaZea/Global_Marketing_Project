2bc02df588c229257817777c7dabe067facf30a0
Stefano <sena.stefano@gmail.com>
2022-10-21 15:19:08 -0400

    4th and 5th iteration failed

--- scripts.js
+++ scripts.js
@@ -7,13 +7,38 @@
 // });
 
 
-
-function changeText(){
-    const userInputFurniture = Array(document.querySelectorAll(".furniture:checked")).map(function(el) {
-            return el.value;
-        }).join(' ')
+// function changeText(){
+//     const userInputFurniture = Array(document.querySelectorAll(".furniture:checked")).map(function(el) {
+//             return el.value;
+//         }).join(' ')
     
 
-    document.getElementById('submit').innerHTML = userInputFurniture;
-    return false;
-    }
+//     document.getElementById('submit').innerHTML = userInputFurniture;
+//     return false;
+//     }
+
+
+// function printText() {
+//     const str = document.getElementsByClassName("furniture");
+//     const selectedTag = "";
+//     for (var i = 0; i < str.length; i++) {
+//         if (str[i].type === "checkbox" && str[i].checked === true) selectedTag += str[i].value + "\n";
+//     }
+//     alert(selectedTag);
+// }
+
+// function getText() {
+//     let text = Array(document.querySelectorAll(".furniture"));
+//     console.log(`${text}`);
+// }
+
+// function printText() {
+//     let print = getText();
+//     console.log(`${print}`);
+// }
+
+function myFunc() {
+    let checkbox = Array(document.querySelectorAll(".furniture:checked"));
+    
+    
+}
